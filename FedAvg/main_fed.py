4a9eb4cc3bcc7354613c22ef3f8f6180c2b855d7
liuying <610618673@qq.com>
2018-12-29 18:35:46 +0800

    delete clip and randomcrop

--- FedAvg/main_fed.py
+++ FedAvg/main_fed.py
@@ -62,8 +62,8 @@ if __name__ == '__main__':
 
     elif args.dataset == 'cifar':
         transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
+            #transforms.RandomCrop(32, padding=4),
+            #transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
