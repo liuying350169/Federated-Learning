6172fcb5e64d0ad1f07bc179638186ff7187ef6f
liuying <610618673@qq.com>
2018-12-31 14:29:01 +0800

    fix lenet

--- FedAvg/FedNets.py
+++ FedAvg/FedNets.py
@@ -62,7 +62,7 @@ class CNNCifar(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return F.log_softmax(x, dim=1)
+        return x
 
 
 ######3######ResNet for cifar10
