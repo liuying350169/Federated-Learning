8f7f04a1be0356910a4dfd6514170ba7d16cd88a
liuying <610618673@qq.com>
2018-12-31 14:24:00 +0800

    add weight_decay=1e-5 in Update

--- FedAvg/Update.py
+++ FedAvg/Update.py
@@ -76,7 +76,7 @@ class LocalUpdate(object):
     def update_weights(self, net):
         net.train()
         # train and update
-        optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=0.5, weight_decay=1e-5)
+        optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=0.9, weight_decay=5e-4)
         epoch_loss = []
         for iter in range(self.args.local_ep):
             batch_loss = []
@@ -101,7 +101,7 @@ class LocalUpdate(object):
 
     def exchange_weight(self, net):
         net.train()
-        optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=0.5, weight_decay=1e-5)
+        optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=0.9, weight_decay=5e-4)
         epoch_loss = []
         #exchange trainset in how many clients
         idxs = self.idxs
