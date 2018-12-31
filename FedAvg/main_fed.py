47f87e8b8f92f9bb9c8da8dc63f9d2725fd2dffc
liuying <610618673@qq.com>
2018-12-31 14:24:57 +0800

    add weight_decay=1e-5 in Update

--- FedAvg/main_fed.py
+++ FedAvg/main_fed.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     elif args.dataset == 'cifar':
         transform_train = transforms.Compose([
             #no affect to the result
-            #transforms.RandomCrop(32, padding=4),
+            transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
