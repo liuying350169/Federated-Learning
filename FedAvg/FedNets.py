f25dd31878e5242445b4be526a049a65f63895b6
liuying <610618673@qq.com>
2019-03-12 09:32:45 +0800

    add fashion mnist datasets

--- FedAvg/FedNets.py
+++ FedAvg/FedNets.py
@@ -64,7 +64,47 @@ class CNNMnist(nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return x
+class CNNModule(nn.Module):
+    def __init__(self):
+        super(CNNModule, self).__init__()
 
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2)
+        self.relu1 = nn.ELU()
+        nn.init.xavier_uniform(self.cnn1.weight)
+
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.relu2 = nn.ELU()
+        nn.init.xavier_uniform(self.cnn2.weight)
+
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.fcl = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        out = self.cnn1(x)
+        out = self.relu1(out)
+        # print ("CNN1")
+        # print (out.size())
+
+        out = self.maxpool1(out)
+        # print ("Maxpool1")
+        # print (out.size())
+
+        out = self.cnn2(out)
+        out = self.relu2(out)
+        # print ("CNN2")
+        # print (out.size())
+        out = self.maxpool2(out)
+        # print ("Maxpool2")
+        # print (out.size(0))
+
+        out = out.view(out.size(0), -1)
+
+        out = self.fcl(out)
+
+        return out
 
 class CNNCifar(nn.Module):
     def __init__(self, args):
